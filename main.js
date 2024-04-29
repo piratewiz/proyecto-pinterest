8a77c652f6fe5e006e364184fd9b6280fa91a62a
piratewiz <mxkst.99@gmail.com>
2024-04-29 22:48:04 +0200

    funciones avanzadas corregidas

--- main.js
+++ main.js
@@ -15,35 +15,42 @@ async function getImages(query) {
     const jsonResponse = await response.json()
     const imagesList = jsonResponse.results
 
-    function createImagesApi(imagesList) {
-      const app = document.querySelector('#app')
-      app.innerHTML = ''
+    const imagesContainer = document.querySelector('.images-container')
+    imagesContainer.innerHTML = ''
 
-      for (let i = 0; i < imagesList.length; i++) {
+    imagesList.forEach((imageData) => {
+      const { height, width, urls } = imageData
+      if (height > width) {
         const image = document.createElement('img')
         const divIMG = document.createElement('div')
-
-        if (imagesList[i].height > imagesList[i].width) {
-          divIMG.className = 'divIMG'
-          image.className = 'everyIMG'
-          image.src = imagesList[i].urls.regular
-
-          divIMG.appendChild(image)
-          app.appendChild(divIMG)
-        }
+        const contentImages = document.createElement('div')
+        contentImages.value = imageData
+        divIMG.className = 'divIMG'
+        image.className = 'everyIMG'
+        image.src = urls.regular
+        divIMG.appendChild(image)
+        contentImages.appendChild(divIMG)
+        imagesContainer.appendChild(contentImages)
       }
-      document.getElementById('#resetButton').style.display = 'block'
-      document.querySelector('.input-search').style.display = 'block'
-    }
+    })
 
-    createImagesApi(imagesList)
+    if (imagesList.length === 0) {
+      createRecommendationButtons()
+      showRecommendedButtons()
+      hideTitleAndParagraph()
+      showErrorMessage()
+    } else {
+      hideTitleAndParagraph()
+    }
   } catch (error) {
     console.error('Error fetching images:', error)
+    showRecommendedButtons()
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   contentApp()
+  createRecommendationButtons()
 })
 
 const contentApp = () => {
@@ -57,8 +64,9 @@ const contentApp = () => {
   divElement.className = 'div-element'
 
   divBtnContainer.className = 'button-container'
+  divBtnContainer.style.display = 'none'
 
-  inputBar.innerHTML = ''
+  inputBar.value = ''
   inputBar.className = 'input-search'
   inputBar.type = 'text'
   inputBar.placeholder = 'Type any Key and press "Enter"'
@@ -68,10 +76,10 @@ const contentApp = () => {
     }
   })
 
-  titlePage.textContent = 'Your free banc wallpapers'
+  titlePage.textContent = 'Explore the Visual Universe'
 
   paragraph.textContent =
-    'Pinterest gives you the tools to build custom agents and workflows in record time '
+    'Welcome to ImageHub, your go-to source for high-quality stock photos, illustrations, and vectors.'
 
   divElement.appendChild(inputBar)
   divElement.appendChild(divBtnContainer)
@@ -79,7 +87,16 @@ const contentApp = () => {
   divElement.appendChild(paragraph)
   app.appendChild(divElement)
 
-  const recommendationButtons = ['forest', 'computers', 'ocean']
+  const imagesContainer = document.createElement('div')
+  imagesContainer.className = 'images-container'
+  app.appendChild(imagesContainer)
+}
+
+function createRecommendationButtons() {
+  const divBtnContainer = document.querySelector('.button-container')
+
+  const recommendationButtons = ['crocodile', 'computers', 'ocean']
+  divBtnContainer.innerHTML = ''
   recommendationButtons.forEach((recommendation) => {
     const button = document.createElement('button')
     button.className = 'btn-recomend'
@@ -93,9 +110,46 @@ const contentApp = () => {
   const resetButton = document.createElement('button')
   resetButton.textContent = 'Reset'
   resetButton.id = 'resetButton'
-  resetButton.style.display = 'none'
   resetButton.addEventListener('click', function () {
     getImages('')
+    showTitleAndParagraph()
+    hideErrorMessage()
   })
   divBtnContainer.appendChild(resetButton)
+
+  showRecommendedButtons()
+}
+
+function showRecommendedButtons() {
+  const divBtnContainer = document.querySelector('.button-container')
+  divBtnContainer.style.display = ''
+}
+
+function hideTitleAndParagraph() {
+  const titlePage = document.querySelector('h1')
+  const paragraph = document.querySelector('p')
+  titlePage.style.display = 'none'
+  paragraph.style.display = 'none'
+}
+
+function showTitleAndParagraph() {
+  const titlePage = document.querySelector('h1')
+  const paragraph = document.querySelector('p')
+  titlePage.style.display = 'block'
+  paragraph.style.display = 'block'
+}
+
+function showErrorMessage() {
+  const app = document.querySelector('#app')
+  const message = document.createElement('h1')
+  message.textContent = 'No results found'
+  message.className = 'error-message'
+  message.style.display = 'block'
+
+  app.appendChild(message)
+}
+
+function hideErrorMessage() {
+  const errorMessage = document.querySelector('.error-message')
+  errorMessage.style.display = 'none'
 }
